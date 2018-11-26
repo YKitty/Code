8367b9291b82a156e47c0390274a05efcfde51dc
centos7.0 <yk@localhost.localdomain>
2018-11-26 01:05:49 -0800

    ""

--- string/capacity/capacity/capacity.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main()
-{
-  std::string str("hello world");
-  std::cout << "the capacity for str: " << str.capacity() << "bytes\n";
-  return 0;
-}
--- string/capacity/max_size/max_size.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-int main()
-{
-  std::string str("hello world");
-  std::cout << "max_size: " << str.max_size() << "bytes \n";
-  return 0;
-}
