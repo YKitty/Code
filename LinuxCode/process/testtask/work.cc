12287a7d2e8c8ae6b3c083877bc5b5632f75d33c
YKitty <991554637@qq.com>
2018-12-02 09:04:31 -0800

    写了关于作业和进程组的区别的代码

---
+++ LinuxCode/process/testtask/work.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main()
+{
+  int i = 5;
+  pid_t id = fork();
+  if (id < 0)
+  {
+    std::cout << "fork error!" << std::endl;
+  }
+  else if (id > 0)//parent
+  {
+    while (i > 0)
+    {
+      std::cout << "parent running..." << std::endl;
+      i--;
+      sleep(1);
+    }
+    exit(0);
+  }
+  else 
+  {
+    while (1)
+    {
+      std::cout << "child running..." << std::endl;
+      sleep(1);
+    }
+  }
+  return 0;
+}
