6ee9a3d3ec615c59f0464c6a79b8cda95b9b1872
YKitty <991554637@qq.com>
2019-01-13 00:11:46 -0800

    书写了智能指针和auto_ptr

---
+++ C++Code/RALL/auto_ptr/auto_ptr.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+
+template<class T>
+class AutoPtr
+{
+public:
+  AutoPtr(T* ptr_)
+    : ptr(ptr_)
+  {
+  }
+
+  ~AutoPtr()
+  {
+    if(ptr)
+    {
+      delete ptr;
+    }
+  }
+
+  T& operator*()
+  {
+    return *ptr;
+  }
+
+  T* operator->()
+  {
+    return ptr;
+  }
+
+  AutoPtr(AutoPtr<T>& t)
+    : ptr(t.ptr)
+  {
+    t.ptr = NULL;
+  }
+
+  T& operator=( AutoPtr<T>& t)
+  {
+    if (this != &t)
+    {
+      if (ptr)
+      {
+        delete ptr;
+      }
+
+      ptr = t.ptr;
+      t.ptr = NULL;
+    }
+
+    return *this;
+  }
+
+private:
+  T* ptr;
+};
+
+struct Date
+{
+  int year;
+  int month;
+  int day;
+};
+
+int main()
+{
+  AutoPtr<Date> d(new Date);
+  d->year = 20;
+  AutoPtr<Date> d1(d);
+  //管理权转移
+  if (d.operator->() == NULL)
+  {
+    std::cout << "null" << std::endl;
+  }
+  return 0;
+}
