1b139360f0bb2c4d801a5b8078ea926139f24309
YKitty <991554637@qq.com>
2019-01-13 05:21:11 -0800

    对于abnormal异常进行修改

--- C++Code/abnormal/abnormal.cpp
+++ C++Code/abnormal/abnormal.cpp
@@ -3,24 +3,69 @@
 
 using namespace std;
 
-int main() throw(exception)
+double Division(int x, int y)
+{
+  if (y == 0)
+  {
+    throw "Divison by zero condition";
+  }
+
+  return (double)x / (double)y;
+}
+
+void Func()
+{
+  int* array = new int[10];
+  try 
+  {
+    int x, y;
+    std::cin >> x >> y;
+    std::cout << Division(x, y) << std::endl;
+  }
+  catch(...)
+  {
+    cout << "delete[]" << array << std::endl; 
+    delete[] array;
+    throw;
+  }
+  
+  std::cout << "delete[]" << array << std::endl;
+  delete[] array;
+}
+
+int main()
 {
   try 
   {
-    vector<int> v(10, 5);
-    v.reserve(1000000);
-    v.at(10);
+    Func();
   }
-  catch (const exception& e)
+  catch(const char* errmsg)
   {
-    cout << e.what() << endl;;
+    std::cout << errmsg <<std::endl;
   }
-  catch (...)
-  {
-    cout << "unkown exception!" << endl;
-  }
-  return 0;
+  return  0;
 }
+
+//using namespace std;
+//
+//int main() throw(exception)
+//{
+//  try 
+//  {
+//    vector<int> v(10, 5);
+//    v.reserve(1000000);
+//    v.at(10);
+//  }
+//  catch (const exception& e)
+//  {
+//    cout << e.what() << endl;;
+//  }
+//  catch (...)
+//  {
+//    cout << "unkown exception!" << endl;
+//  }
+//  return 0;
+//}
 
 
 //double Division(int x, int y) throw(const char*)
