cefa0720025dc9283c884cdc6b6f8f6672b1ca41
YKitty <991554637@qq.com>
2019-01-16 07:07:44 -0800

    项目代理服务器框架

---
+++ Demo/Server/epoll.hpp
@@ -0,0 +1,20 @@
+#define __SOCKS5__
+#ifdef __SOCKS5__ 
+
+class EpollServer
+{
+public:
+  EpollServer()
+  {
+
+  }
+
+protected:
+
+};
+
+
+
+
+
+#endif //__SOCKS5__ 
---
+++ Demo/Server/socks5.hpp
@@ -0,0 +1,22 @@
+#define __SOCKS5__
+#ifdef __SOCKS5__ 
+
+#include "epoll.hpp"
+
+class Sock5Server : public EpollServer
+{
+public:
+  Sock5Server()
+  {
+
+  }
+
+protected:
+
+};
+
+
+
+
+
+#endif //__SOCKS5__ 
