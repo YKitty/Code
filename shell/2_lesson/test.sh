7eff9c0b3388d5194f0642ec958d89b9f111415e
YKitty <991554637@qq.com>
2019-01-23 07:07:48 -0800

    学习shell

--- shell/2_lesson/test.sh
+++ shell/2_lesson/test.sh
@@ -1,7 +1,131 @@
 #!/bin/bash 
 
 
-echo $RANDOM
+read myint
+[ $myint -eq 100 ] && echo "yes 100"
+[ $myint -eq 200 ] || echo "no 200"
+
+#aread myint 
+#if /bin/true ;then
+#  :
+#else 
+#  echo "noequal"
+#fi
+
+#read myint 
+#if [ $myint -eq 100 ];then
+#  echo "equal"
+#else 
+#  echo "noequal"
+#fi
+#
+#read filename
+#if grep '222' "$filename";then
+#  echo "yes"
+#else
+#  echo "no"
+#fi
+
+
+#read myint 
+#if [ $myint -le 100 ];then
+#  echo "le 100"
+#  if [ $myint -le 50 ];then 
+#    echo "le 50"
+#  elif [ $myint -ge 50 ];then 
+#    echo "ge 50"
+#  else 
+#    echo "myint in 50 ~ 100"
+#  fi 
+#else
+#  echo "gt 100"
+#fi
+
+#read mystring
+#if [ "$mystring" == "hello" ];then
+#  echo "hello"
+#fi
+
+#read d1 d2 
+#[ $d1 -eq 100 -o $d2 -eq 200 ]
+#echo $?
+
+#read d1 d2 
+#[ $d1 -eq 100 -a $d2 -eq 200 ]
+#echo $?
+
+#read mystring
+#[ ! "$mystring" == "hello" ]
+#echo $?
+#
+#[ -c /dev/tty ]
+#echo $?
+#[ -b /dev/sda ]
+#echo $?
+#[ -f ./test.sh ]
+#echo $?
+#[ -d / ]
+#echo $?
+
+#read mystring
+#
+#[ "$mystring" == "hello" ]
+#echo $?
+#
+
+
+#read mystring 
+#[ $mystring == "helloworld" ]
+#echo $?
+#[ $mystring != "helloworld" ]
+#echo $?
+#[ -z $mystring ]
+#echo $?
+#[ -n $mystring ]
+#echo $?
+#
+
+#read myint
+#[ $myint -eq 100 ] 
+#echo $?
+#[ $myint -ne 100 ] 
+#echo $?
+#[ $myint -lt 100 ] 
+#echo $?
+#[ $myint -gt 100 ] 
+#echo $?
+
+
+#read myint
+#test $myint -eq 100
+#echo $?
+#
+
+
+#myint=520
+#echo $myint 
+#ret=$((myint++))
+#echo $myint
+
+
+#echo `date +%Y`
+#echo $(date +%Y)
+
+#your_name="yk"
+#echo ${#your_name}
+#echo "hello, $your_name"
+#echo "hello, ${your_name}"
+#echo 'hello, '$your_name''
+#echo 'hello, ${your_name}'
+
+
+#echo $a
+
+#pwd
+#cd ..
+#pwd
+
+#echo $RANDOM
 
 
 #-e开启转义，-c不换行
