a47443af01eb02d1bc57d9b45c1c21c4dc3fc014
centos7.0 <yk@localhost.localdomain>
2019-03-24 07:46:45 -0700

    write all code AVLTree and true test

--- C++Code/AVLAndRBTree/AVLTree.hpp
+++ C++Code/AVLAndRBTree/AVLTree.hpp
@@ -45,7 +45,8 @@ public:
     Node* pparent = parent->_parent;
     parent->_parent = subtree_right;
     
-    if (pparent == _root)
+    subtree_right->_parent = pparent;
+    if (parent == _root)
     {
       _root = subtree_right;
     }
@@ -60,7 +61,6 @@ public:
         pparent->_right = subtree_right;
       }
     }
-    subtree_right->_parent = pparent;
 
     subtree_right->_bf = parent->_bf = 0;
   }
@@ -68,18 +68,19 @@ public:
   void RightRotate(Node* parent)
   {
     Node* subtree_left = parent->_left;
-    Node* subtree_left_right = subtree_left;
+    Node* subtree_left_right = subtree_left->_right;
 
     parent->_left = subtree_left_right;
     if (subtree_left_right)
     {
-      subtree_left_right = parent;
+      subtree_left_right->_parent = parent;
     }
 
     subtree_left->_right = parent;
     Node* pparent = parent->_parent;
     parent->_parent = subtree_left;
 
+    subtree_left->_parent = pparent;
     if (parent == _root)
     {
       _root = subtree_left;
@@ -95,26 +96,49 @@ public:
         pparent->_right = subtree_left;
       }
     }
-    subtree_left->_parent = pparent;
 
     subtree_left->_bf = parent->_bf = 0;
   }
 
   void LeftRightRotate(Node* parent)
   {
+    Node* subtree_left = parent->_left;
+    int mid_node_bf = parent->_left->_right->_bf;
+
     LeftRotate(parent->_left);
     RightRotate(parent);
+
+    if (mid_node_bf == -1)
+    {
+      parent->_bf = -1;
+    }
+    if (mid_node_bf == 1)
+    {
+      subtree_left->_bf = 1;
+    }
   }
 
   void RightLeftRotate(Node* parent)
   {
+    Node* subtree_right = parent->_right;
+    int mid_node_bf = parent->_right->_left->_bf;
+
     RightRotate(parent->_right);
     LeftRotate(parent);
+
+    if (mid_node_bf == 1)
+    {
+      subtree_right->_bf = -1;
+    }
+    if (mid_node_bf == -1)
+    {
+      parent->_bf = 1;
+    }
   }
 
   bool Insert(const K& k, const V& v)
   {
-    if (_root)
+    if (_root == nullptr)
     {
       _root = new Node(k, v);
       return true;
@@ -133,26 +157,25 @@ public:
       {
         //右子树
         parent = cur;
-        cur = cur->_left;
+        cur = cur->_right;
       }
       else 
       {
         //左子树
         parent = cur;
-        cur = cur->_right;
+        cur = cur->_left;
       }
     }
     //表示找到了插入的位置，进行插入
     if (parent->_key > k)
     {
       parent->_left = insert_node;
-      insert_node->_parent = parent;
     }
     else 
     {
       parent->_right = insert_node;
-      insert_node->_parent = parent;
     }
+    insert_node->_parent = parent;
 
     //插入完毕之后需要判断这个数还是不是AVL树，如果不是的话需要进行调整
     //调整
@@ -186,13 +209,39 @@ public:
       {
         //高度改变，一直向上调整
         insert_node = parent;
-        parent = parent->_parent;
+        parent = insert_node->_parent;
       }
       else if (abs(parent->_bf) == 2)
       {
         //需要进行旋转
         //四种类型的旋转
         
+        if (parent->_bf == -2)
+        {
+          if (parent->_left->_bf == -1)
+          {
+            //右单旋
+            RightRotate(parent);
+          }
+          else 
+          {
+            //左右单旋
+            LeftRightRotate(parent);
+          }
+        }
+        else if (parent->_bf == 2)
+        {
+          if (parent->_right->_bf == 1)
+          {
+            //左单旋
+            LeftRotate(parent);
+          }
+          else 
+          {
+            //右左单旋
+            RightLeftRotate(parent);
+          }
+        }
         break;//更新结束直接不再更新
       }
       else 
@@ -201,11 +250,57 @@ public:
         assert(false);
       }
     }
+  }
 
+  void InOrderSubFunction(Node* root)
+  {
+    if (root == nullptr)
+    {
+      return ;
+    }
 
+    InOrderSubFunction(root->_left);
+    std::cout << root->_key << " ";
+    InOrderSubFunction(root->_right);
+  }
 
+  void InOrder()
+  {
+    InOrderSubFunction(_root);
+    std::cout << std::endl;
+  }
 
+  int Height(Node* root)
+  {
+    if (root == nullptr)
+    {
+      return 0;
+    }
 
+    int left = Height(root->_left);
+    int right = Height(root->_right);
+
+    return left > right ? left + 1 : right + 1;
+  }
+
+  bool IsBalanceSubFunction(Node* root)
+  {
+    if (root == nullptr)
+    {
+      return true;
+    }
+
+    int left = Height(root->_left);
+    int right = Height(root->_right);
+
+    return (abs(right - left) < 2) \
+      && (IsBalanceSubFunction(root->_left)) \
+      && (IsBalanceSubFunction(root->_right));
+  }
+
+  bool IsBalance()
+  {
+    return IsBalanceSubFunction(_root);
   }
 
 private:
