e354ca963aca2dad9e30c51271f7c1868f3b9a77
centos7.0 <yk@localhost.localdomain>
2019-05-21 23:41:41 +0800

    nullptr not have address right quote

--- C++Code/C++11/nullptr/nullptr.cc
+++ C++Code/C++11/nullptr/nullptr.cc
@@ -5,6 +5,55 @@
 using namespace std;
 
 /******************************
+ * nullptr_t对象取地址
+ * nullptr不能取地址
+ *****************************/ 
+/*
+int main()
+{
+    nullptr_t my_null;
+    printf("%x\n", &my_null);
+
+    //printf("%x\n", &nullptr); // 无法编译通过
+    printf("%d\n", my_null == nullptr);
+
+    const nullptr_t&& default_nullptr = nullptr; // 右值引用
+    printf("%x\n", &default_nullptr);
+
+    return 0;
+}
+*/   
+/******************运行结果************************/
+/* 
+ * 56c25338
+ * 1
+ * 56c25340
+ */ 
+
+
+/******************************
+ * nullptr_t应用于模板
+ *****************************/ 
+/*
+template<typename T>
+void g(T* t){}
+
+template<typename T>
+void h(T t){}
+
+int main()
+{
+    g(nullptr); // 编译失败，nullptr的类型是nullptr_t而不是指针
+    g((float*)nullptr); // 推导出T = float 
+
+    h(0); // 推导出 T = int
+    h(nullptr); // 推导出 T = nullptr_t 
+    h((float*)nullptr); // 推导出 T = float*
+    return 0;
+}
+*/ 
+
+/******************************
  * nullptr_t类型规则：
  * 1. 所有定义为nullptr_t类型的数据都是等价的
  * 2. nullptr_t类型的数据可以隐式转换为任意一个指针类型
@@ -12,6 +61,7 @@ using namespace std;
  * 4. nullptr_t类型的数据不能进行算术运算
  * 5. nullptr_t类型的数据可以进行关系运算，但仅可以与指针类型数据进行比较。仅能使用==,>=,<=等运算符为true
  *****************************/ 
+/*
 int main()
 {
     nullptr_t nul;
@@ -44,6 +94,7 @@ int main()
 
     return 0;
 }
+*/ 
 
 /******************************
  * nullptr：指针空值，仅仅只是对于一个空指针，可以隐式类型转换为要赋值的指针类型
